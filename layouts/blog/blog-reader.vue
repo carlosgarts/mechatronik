9d4748ff3d814e3f1b80d2ae9b67e68be3264943
Carlos <carlosgarts@gmail.com>
2020-01-12 04:06:35 -0400

    formateado el contenido

--- layouts/blog/blog-reader.vue
+++ layouts/blog/blog-reader.vue
@@ -5,7 +5,12 @@
       <div class="titulo" v-bind:style="{ backgroundImage: 'url(' + post.featured_image_url + ')' }">
         <h2>{{ post.title.rendered }}</h2>
       </div>
-      <div class="content" v-html="post.content.rendered"></div>
+      <div class="content" >
+        <div class="social">
+
+        </div>
+        <div class="format" v-html="post.content.rendered"></div>
+      </div>
     </div>
   </div>
 </template>
@@ -94,86 +99,13 @@ export default {
   }
 
   .content {
-    width: 60%;
-    margin: 150px;
+    width: 65%;
+    margin-left: 17.5%;
+    margin-right: 17.5%;
     margin-top: 0px;
+    margin-bottom: 0px;
     padding: 80px;
     padding-top: 20px;
-  }
-
-  .service-content {
-    width: 100%;
-    min-height: 60vh;
-    display: grid;
-    grid-template-columns: 1fr;
-    z-index: 5;
-    position: relative;
-    margin-top: 50px;
-    margin-bottom: 100px;
-    @media (min-width: 700px) {
-      grid-template-columns: 1fr 1fr;
+    overflow: hidden;
     }
-    .text {
-      text-align: right;
-      padding-left: 7.5%;
-      padding-right: 7.5%;
-      display: flex;
-      flex-flow: column;
-      justify-content: center;
-      align-items: flex-end;
-      .tbb {
-        margin-left: 0;
-        margin-right: 0;
-        border-color: black;
-        h2 {
-          transition: 0.5s;
-          color: black;
-          border-color: black;
-          @media (max-width: 400px) {
-            font-size: 30px;
-          }
-        }
-      }
-      p {
-        line-height: 1.5;
-      }
-    }
-    .image {
-      height: 300px;
-      width: 85%;
-      overflow: hidden;
-      margin: auto;
-      margin-top: 20px;
-      @media (min-width: 700px) {
-        height: 85%;
-        width: 85%;
-        margin: auto auto;
-      }
-      img {
-        transition: transform 10s ease;
-        width: 100%;
-        height: 100%;
-        object-fit: cover;
-      }
-      img:hover {
-        transform: scale(1.3);
-      }
-    }
-  }
-  .service-content:nth-child(2n) {
-    .text {
-      text-align: left;
-      align-items: flex-start;
-      @media (min-width: 700px) {
-        grid-column: 2 / 3;
-      }
-    }
-    .image {
-      @media (min-width: 700px) {
-        grid-column: 1 / 2;
-        grid-row: 1;
-      }
-    }
-  }
-
 </style>
